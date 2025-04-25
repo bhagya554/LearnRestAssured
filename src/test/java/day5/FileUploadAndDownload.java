9894b9584bbbac6a62b993509b8ac9f86ae21a93
bhagya554 <kudupudi.bhagya@gmail.com>
2025-04-25 14:58:37 +0530

    File Upload

---
+++ src/test/java/day5/FileUploadAndDownload.java
@@ -0,0 +1,46 @@
+package day5;
+
+import java.io.File;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class FileUploadAndDownload {
+	@Test
+	public void singleFileUpload() {
+		File myFile = new File("./src/test/resources/day5/Test 1.txt");
+		given()
+			.multiPart("file", myFile)
+			.contentType("multipart/formdata")
+		.when()
+			.post("http://localhost:8080/uploadFile")
+		.then()
+			.statusCode(200)
+			.body("fileName", equalTo("Test 1.txt"));
+	}
+	
+	@Test
+	public void multipleFileUpload() {
+		File myFile1 = new File("./src/test/resources/day5/Test 1.txt");
+		File myFile2 = new File("./src/test/resources/day5/Test 2.txt");
+		File[] fileArr= {myFile1,myFile2};
+		
+		given()
+				/*
+				 * .multiPart("files", myFile1) .multiPart("files",myFile2)
+				 */
+			.multiPart("files",fileArr)
+			.contentType("multipart/formdata")
+		.when()
+			.post("http://localhost:8080/uploadMultipleFiles")
+		.then()
+			.statusCode(200)
+			.body("[0].fileName", equalTo("Test 1.txt"))
+			.body("[1].fileName", equalTo("Test 2.txt"))
+			.log().all();
+	}
+	
+
+}
